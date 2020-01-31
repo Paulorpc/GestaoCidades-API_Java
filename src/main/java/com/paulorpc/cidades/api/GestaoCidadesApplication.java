952f7458421a4be99aafb08075d7497be83d3117
paulorpc <paulorpc@gmail.com>
2020-01-31 15:08:18 -0300

    Comminit inicial do projeto

---
+++ src/main/java/com/paulorpc/cidades/api/GestaoCidadesApplication.java
@@ -0,0 +1,13 @@
+package com.paulorpc.cidades.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCidadesApplication.class, args);
+	}
+
+}
