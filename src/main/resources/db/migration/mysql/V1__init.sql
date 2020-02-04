fc7b308bca7e8064e767038ce84dcdc3f30cbd39
paulorpc <paulorpc@gmail.com>
2020-02-04 01:36:58 -0300

    problema no git, refazendo merge.

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cidade (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	ibge_id INT NOT NULL UNIQUE,
+	uf CHAR(2) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	capital BOOLEAN,
+	lon DECIMAL(11, 8),
+	lat DECIMAL(10, 8),
+	no_accents VARCHAR(50),
+	alternative_name VARCHAR(50),
+	microregion VARCHAR(50),
+	mesoregion VARCHAR(50),
+	dateEntry DATETIME NOT NULL,
+	dateUpdate DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/test/java/com/paulorpc/cidades/api/GestaoCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulorpc.cidades.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
